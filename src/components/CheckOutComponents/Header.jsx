db5ddeff7081d40e66e1ec5315a1669acb14e3cf
robert <robertrenbysanjuan@gmail.com>
2025-06-13 07:41:51 +0800

    feat: add header in checkout

--- src/components/CheckOutComponents/Header.jsx
+++ src/components/CheckOutComponents/Header.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from 'react'
 import lockIcon from '../../assets/icons/checkout-lock-icon.png'
 
 export default function Header(){
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 620)
+
+    useEffect(()=>{
+        const handleResize = ()=>{   
+            window.innerWidth <= 620 ? setIsMobile(true) : setIsMobile(false)
+        }
+        window.addEventListener('resize', handleResize)   
+
+        return ()=> window.removeEventListener('resize', handleResize)
+    })
     
     return(
         <header className="checkOutHeader">
-            <img src="/logos/amazon-logo.png" alt="amazon logo" />
+            <img 
+                src={isMobile ? '/logos/amazon-mobile-logo.png' : 
+                                '/logos/amazon-logo.png'} 
+                alt="amazon logo"
+                className={isMobile ? 'mobile' : 'window'}/>
 
             <div>
                 <p>Checkout (<span> 0 items </span>)</p>
@@ -14,3 +29,5 @@ export default function Header(){
         </header>
     )
 }
+
+//amazon-mobile-logo.png

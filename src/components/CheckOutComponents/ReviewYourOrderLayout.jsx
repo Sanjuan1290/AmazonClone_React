df70100e4fd7e64c591dcb0f1a0a9fc2c3d3ddf0
robert <robertrenbysanjuan@gmail.com>
2025-06-13 09:46:21 +0800

    refactor: change some codes

--- src/components/CheckOutComponents/ReviewYourOrderLayout.jsx
+++ src/components/CheckOutComponents/ReviewYourOrderLayout.jsx
@@ -1,11 +1,12 @@
 import YourOrder from "./YourOrder"
 import OrderSummary from "./OrderSummary"
+import { useEffect, useState } from "react"
 
 export default function ReviewYourOrderLayout(){
 
     return(
-        <main className="checkout-orderReview-container">
-            <div className="review-order-container">
+        <main className={`checkout-orderReview-container`}>
+            <div className={`review-order-container `}>
                 <h2>Review your order</h2>
                  <YourOrder />
             </div>

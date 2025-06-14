13aafe671c1e1a9e74a33e1db4f2d829f1e7dd8b
robert <robertrenbysanjuan@gmail.com>
2025-06-14 08:42:10 +0800

    feat: save cart in local storage

--- src/cart.js
+++ src/cart.js
@@ -1,21 +1,15 @@
 
-export const cart = [
-    {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 1
-    },
-    {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 2
-    }
-]
+export let cart = JSON.parse(localStorage.getItem('cart')) || []
 
 
 export function addToCart(productId, quantity){
     const existingProduct = cart.find(item => item.productId === productId)
 
     existingProduct ? existingProduct.quantity += quantity : cart.push({ productId, quantity })
+
+    saveCart()
 }
+
 
 export function getTotalQuantity(){
     let totalQuantity = 0;
@@ -24,4 +18,9 @@ export function getTotalQuantity(){
     })
 
     return totalQuantity
+}
+
+
+export function saveCart(){
+    localStorage.setItem('cart', JSON.stringify(cart))
 }
